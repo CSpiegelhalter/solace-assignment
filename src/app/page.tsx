e25cf4deba190671d7f38e787a5b15ca438f6434
CSpiegelhalter <spiegelhaltercurt@gmail.com>
2025-08-27 10:24:13 -0700

    Fixed hydration error. Added typing for advocate. Edited the search filter to useMemo to avoid needless processing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,60 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
+
+type Advocate = {
+  id: string | number;
+  firstName: string;
+  lastName: string;
+  city: string;
+  degree: string;
+  specialties: string[];
+  yearsOfExperience: number | string;
+  phoneNumber: string;
+};
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
+  const [advocates, setAdvocates] = useState<Advocate[]>([]);
+  const [searchTerm, setSearchTerm] = useState("")
 
   useEffect(() => {
     console.log("fetching advocates...");
     fetch("/api/advocates").then((response) => {
       response.json().then((jsonResponse) => {
         setAdvocates(jsonResponse.data);
-        setFilteredAdvocates(jsonResponse.data);
+        // setFilteredAdvocates(jsonResponse.data);
       });
     });
   }, []);
 
-  const onChange = (e) => {
-    const searchTerm = e.target.value;
+  const filteredAdvocates = useMemo(() => {
 
-    document.getElementById("search-term").innerHTML = searchTerm;
+    const formattedTerm = searchTerm?.trim()?.toLowerCase()
 
-    console.log("filtering advocates...");
-    const filteredAdvocates = advocates.filter((advocate) => {
-      return (
-        advocate.firstName.includes(searchTerm) ||
-        advocate.lastName.includes(searchTerm) ||
-        advocate.city.includes(searchTerm) ||
-        advocate.degree.includes(searchTerm) ||
-        advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
-      );
-    });
+    if (!formattedTerm) return advocates
 
-    setFilteredAdvocates(filteredAdvocates);
-  };
+    const has = (term: unknown) => {
+      return String(
+        Array.isArray(term) ? term.join(' ') : term ?? ''
+      ).toLowerCase().includes(formattedTerm)
+    }
+
+    return advocates.filter((e) =>
+      has(e.firstName) ||
+      has(e.lastName) ||
+      has(e.city) ||
+      has(e.degree) ||
+      has(e.specialties) ||
+      has(e.yearsOfExperience) ||
+      has(e.phoneNumber)
+    )
+
+  }, [advocates, searchTerm])
+
 
   const onClick = () => {
     console.log(advocates);
-    setFilteredAdvocates(advocates);
+    // setFilteredAdvocates(advocates);
   };
 
   return (
@@ -51,24 +67,27 @@ export default function Home() {
         <p>
           Searching for: <span id="search-term"></span>
         </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
+        <input style={{ border: "1px solid black" }} onChange={(e) => setSearchTerm(e.target.value)} />
         <button onClick={onClick}>Reset Search</button>
       </div>
       <br />
       <br />
       <table>
         <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>City</th>
+            <th>Degree</th>
+            <th>Specialties</th>
+            <th>Years of Experience</th>
+            <th>Phone Number</th>
+          </tr>
         </thead>
         <tbody>
           {filteredAdvocates.map((advocate) => {
             return (
+
               <tr>
                 <td>{advocate.firstName}</td>
                 <td>{advocate.lastName}</td>
@@ -78,10 +97,12 @@ export default function Home() {
                   {advocate.specialties.map((s) => (
                     <div>{s}</div>
                   ))}
+
                 </td>
                 <td>{advocate.yearsOfExperience}</td>
                 <td>{advocate.phoneNumber}</td>
               </tr>
+
             );
           })}
         </tbody>
